0d2cd8964da74210fe3d1c8a601b47493bd49b35
IIaJIeHo <i.dol.market@gmail.com>
2016-01-15 19:41:49 +0300

    localstorage +  delete  + complete

--- creative.js
+++ creative.js
@@ -1,10 +1,10 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function($scope) {
-    $scope.todos = [
-      {title:'learn angular', done:true},
-      {title:'build an angular app', done:false}];
     $scope.reverse = true;
     $scope.changed = null;
+    $scope.saved = localStorage.getItem('todos');
+    $scope.todos = $scope.saved == null ? [] : JSON.parse($scope.saved);
+    localStorage.setItem('todos', JSON.stringify($scope.todos));
 
     $scope.reverseOrder = function(){
         $scope.reverse = !$scope.reverse;
@@ -27,16 +27,25 @@ angular.module('todoApp', [])
       return count;
     };
 
-    $scope.change = function(todo){
+    $scope.changeTodo = function(todo){
       $scope.changed = todo;
       $scope.todoText = todo.title;
     }
+    //delete on double click
+    $scope.deleteTodo = function(todo){
+      $scope.todos.splice($scope.todos.indexOf(todo),1);
+    }
  
-    $scope.archive = function() {
-      var oldTodos = todoList.todos;
+    $scope.deleteAutos = function() {
+      var oldTodos = $scope.todos;
       $scope.todos = [];
       angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
+        if (!todo.done) $scope.todos.push(todo);
       });
     };
+
+    $scope.$watch('todos', function(newVal, oldVal){
+      localStorage.setItem('todos', JSON.stringify(newVal));
+  }, true);
+
   });
